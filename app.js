b23ef448d9c711c6d29608f3f444298de20adcbe
ColeBrian-FS <70656615+ColeBrian-FS@users.noreply.github.com>
2021-04-27 14:22:41 -0400

    Updated HTML CSS and JS files

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 function onSubmit(){
     let n = document.getElementById("Name").value;
     let g = document.getElementById("Gamertag").value;
-    let s = document.getElementById("System").value;
-    let a = document.getElementById("Game").value;
+    let s = document.getElementById("System");
+    let a = document.getElementById("Game");
     let name  = n;
     let gamertag = g;
-    let systemSelect = s;
-    let gameSelect = a;
+
+    let results1 = s.options[s.selectedIndex].text;
+    let results2 = a.options[a.selectedIndex].text;
     document.getElementById("name-output").innerHTML = "Name: " + name;
     document.getElementById("gamertag-output").innerHTML = "Gamertag: " + gamertag; 
-    document.getElementById("system-output").innerHTML = "System: " + systemSelect;
-    document.getElementById("game-output").innerHTML = "Game: " + gameSelect;
+    document.getElementById("system-output").innerHTML = "System: " + results1;
+    document.getElementById("game-output").innerHTML = "Game: " + results2;
  }
